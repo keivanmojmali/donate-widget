de2041aa5c8463cf3800dd0d2dd2719666221313
keivanmojmali <mojmalik@gmail.com>
2021-03-08 18:32:11 -0800

    working on render

--- client/pages/widget.jsx
+++ client/pages/widget.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
 
-export default function Home(props) {
-  return (
-    <>
-    </>
-  );
+export default class Widget extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      firstName: ' ',
+      lastName: ' ',
+      amount: ' ' ,
+
+    };
+  };
+  render(){
+    return(
+      <div className="container">
+
+        <div className="row">
+          <div className="col">
+            <h4>Donate Now</h4>
+          </div>
+          <div className="col-sm">
+            <h2>X</h2>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col">
+            <h1>Donate to Petlanthropy</h1>
+          </div>
+        </div>
+
+
+      </div>
+    )
+  }
 }
