30d1910f517328e39b3252052f11f33409695e2c
keivanmojmali <mojmalik@gmail.com>
2021-03-08 18:19:16 -0800

    working on rendering

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Home from './pages/home';
+import Widget from './pages/widget';
 
 export default class App extends React.Component {
   render() {
-    return <Home />;
+    return <Widget />;
   }
 }
