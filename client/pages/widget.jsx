e486e7d2f37c53ee29452927c8da1ff053a7a32a
keivanmojmali <mojmalik@gmail.com>
2021-03-23 21:00:37 -0700

    working onit

--- client/pages/widget.jsx
+++ client/pages/widget.jsx
@@ -28,12 +28,12 @@ export default class Widget extends React.Component {
     const value = event.target.value;
     const donationInfo = this.state.donationInfo;
     donationInfo[parameter] = value;
-    this.setState({donationInfo})
+    this.setState({ donationInfo })
   }
-  submit(){
+  submit() {
     event.preventDefault();
     console.log(this.state.donationInfo)
-    this.setState({'creditView': 'thankYou'})
+    this.setState({ 'creditView': 'thankYou' })
   }
   donationStage() {
     if (this.state.creditView === true) {
@@ -42,19 +42,19 @@ export default class Widget extends React.Component {
           <div className="row">
             <div className="col d-flex flex-column">
               <label htmlFor="firstName">First Name</label>
-              <input type="text" className='form-input' name='firstName' value={this.state.firstName} onChange={this.onChange} required/>
+              <input type="text" className='form-input' name='firstName' value={this.state.firstName} onChange={this.onChange} required />
             </div>
           </div>
           <div className="row">
             <div className="col d-flex flex-column">
               <label htmlFor="lastName">Last Name</label>
-              <input type="text" className='form-input' name='lastName' value={this.state.lastName} onChange={this.onChange} required/>
+              <input type="text" className='form-input' name='lastName' value={this.state.lastName} onChange={this.onChange} required />
             </div>
           </div>
           <div className="row">
             <div className="col d-flex flex-column">
               <label htmlFor="creditCard">CC #</label>
-              <input type="number" value={this.state.creditCard} name='creditCard' onChange={this.onChange} required/>
+              <input type="number" value={this.state.creditCard} name='creditCard' onChange={this.onChange} required />
             </div>
           </div>
           <div className="row mt-3">
@@ -65,26 +65,26 @@ export default class Widget extends React.Component {
         </form>
       )
     } else if (this.state.creditView === false) {
-    return (
-      <div className='col'>
-        <div className="row d-flex align-items-center justify-content-center">
-          <div className="col">
-            <button className='btn w-100 btn-primary' onClick={() => { this.handleClick(10) }}>$10</button>
+      return (
+        <div className='col'>
+          <div className="row m-0 d-flex align-items-center justify-content-center">
+            <div className="col p-1 ">
+              <button className='square-button' onClick={() => { this.handleClick(10) }}>$10</button>
+            </div>
+            <div className="col p-1 ">
+              <button className='square-button' onClick={() => { this.handleClick(30) }}>$30</button>
+            </div>
+            <div className="col p-1 ">
+              <button className='square-button' onClick={() => { this.handleClick(50) }}>$50</button>
+            </div>
           </div>
-          <div className="col">
-            <button className='btn w-100 btn-primary' onClick={() => { this.handleClick(30) }}>$30</button>
-          </div>
-          <div className="col">
-            <button className='btn w-100 btn-primary' onClick={() => { this.handleClick(50) }}>$50</button>
+          <div className="row m-0 ">
+            <div className="col p-1">
+              <button className='bottom-rectangle' onClick={() => this.handleClick('15 Monthly')}>$15 Monthly Donation</button>
+            </div>
           </div>
         </div>
-        <div className="row text-center">
-          <div className="col">
-            <button className='btn w-100  btn-primary' onClick={() => this.handleClick('15 Monthly')}>$15 Monthly Donation</button>
-          </div>
-        </div>
-      </div>
-    )
+      )
     } else {
       return (
         <div className="col">
@@ -96,23 +96,38 @@ export default class Widget extends React.Component {
   render() {
     console.log(this.state)
     return (
-      <div className="container-fluid p-3">
-        <div className="row border-bottom border-danger">
+      <div className="container-fluid p-3 h-100 m-0">
+        <div className="row border-bottom border-danger m-0">
           <div className="col">
             <img className='w-50 pb-2' src='https://static.wixstatic.com/media/577460_c902da9df2784a74bcba2e68f1dda16b~mv2.png/v1/fill/w_279,h_66,al_c,q_85,usm_0.66_1.00_0.01/Petlanthropy-Logo-2b.webp'
-            alt="Petlanthropy Logo"/>
+              alt="Petlanthropy Logo" />
           </div>
         </div>
-        <div className="row">
+        <div className="row m-0">
           <div className="col text-center">
             <h1>Donate to Petlanthropy</h1>
           </div>
         </div>
 
-        <div className="row">
+        <div className="row m-0">
           {this.donationStage()}
         </div>
 
+        <div className="row m-0 d-flex">
+          <div className="col text-left d-flex">
+            <p className='small-font mr-1'>
+              Stay Informed
+          </p>
+            <p className='text-primary small-font'>
+              Newsletter Sign Up
+            </p>
+          </div>
+          <div className="col">
+            <p className='text-primary small-font text-right'>
+              Terms and Conditions
+            </p>
+          </div>
+        </div>
       </div>
     )
   }
